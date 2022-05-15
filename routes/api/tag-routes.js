f486927c47e1a63d1eb053c0a903eff81f17b5ad
Primitive J <hokecj@gmail.com>
2022-05-15 11:25:17 -0400

    initial commit - DID ENTIRE ACTIVITY IN FRONT OF CLASS/WITH INSTRUCTOR

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model:Product, through: ProductTag}]
+    })
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include:  [{model:Product, through: ProductTag}]
+    })
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  // be sure to include its associated Product data
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const newTag = await Tag.create(
+      req.body
+    )
+    res.status(200).json(newTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try{
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+  res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy( {
+      where: {
+        id:req.params.id
+      }
+    })
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  // delete one product by its `id` value
+});
+
+module.exports = router;
